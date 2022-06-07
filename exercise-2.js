79d5558f683ca99bdbb462d817b63bba583dd5b1
ndrSaba <102291579+ndrSaba@users.noreply.github.com>
2022-06-07 15:35:46 +0200

    es fix

--- exercise-2.js
+++ exercise-2.js
@@ -1,6 +1,6 @@
 const order = {};
 
-const customerCity = order?.city ?? "City is required";
+const customerCity = order?.customer?.city ?? "City is required";
 console.log(customerCity);
 
 // if (order && order.customer && order.customer.address && !order.customer.address.city) {
